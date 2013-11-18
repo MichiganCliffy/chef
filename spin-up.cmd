e02afd36dc0fbb252787aa22c67cb2b8e0a41705
William Clifford <wclifford@wine.com>
2013-11-17 20:12:08 -0800

    ???

--- spin-up.cmd
+++ spin-up.cmd
@@ -1,2 +1,2 @@
 vagrant up
-knife bootstrap localhost --ssh-user vagrant --ssh-password vagrant --ssh-port 2222 --run-list "recipe[apt],recipe[vim],recipe[users],recipe[java],recipe[openssl]" --sudo
+knife bootstrap localhost --ssh-user vagrant --ssh-password vagrant --ssh-port 2222 --run-list "recipe[apt],recipe[vim],recipe[users],recipe[java],recipe[openssl],recipe[maven],recipe[runit],recipe[git]" --sudo
