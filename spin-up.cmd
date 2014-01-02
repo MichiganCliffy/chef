78cc952c40c24aba20981ce9eb33575cbedce6d7
William Clifford <wclifford@wine.com>
2014-01-01 20:09:26 -0800

    checking in bootstrap script

--- spin-up.cmd
+++ spin-up.cmd
@@ -1,2 +1,2 @@
 vagrant up
-knife bootstrap localhost --ssh-user vagrant --ssh-password vagrant --ssh-port 2222 --run-list "recipe[apt],recipe[vim],recipe[users],recipe[java],recipe[scala],recipe[openssl],recipe[runit],recipe[git],recipe[mercurial],recipe[maven],recipe[mongodb]" --sudo
+knife bootstrap localhost --ssh-user vagrant --ssh-password vagrant --ssh-port 2222 --run-list "recipe[apt],recipe[vim],recipe[users],recipe[runit],recipe[git],recipe[mercurial]" --sudo
